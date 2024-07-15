8ed62ad444287df36fa09ddb07258f7a15230ff2
hongmoxian <2428253335@qq.com>
2024-07-16 00:56:36 +0800

    部分代码添加注释

--- vaspworkflow/flow.py
+++ vaspworkflow/flow.py
@@ -10,6 +10,18 @@ from ase.io import read
 from tqdm import tqdm
 
 class VASPManager:
+    """
+    Class to manage VASP calculations
+
+    Args:
+    work_path: path to the working directory
+    vasp_sub_path: path to the VASP submission script
+    structre_db_path: path to the structure database
+    max_jobs: maximum number of jobs to run at once
+    sleep_time: time to sleep between checks
+    user_name: user name for the VASP submission script
+    prefix: prefix for the structure files
+    """
     def __init__(self, work_path, vasp_sub_path, structre_db_path, max_jobs, sleep_time, user_name, prefix):
         self.work_path = work_path
         self.vasp_sub_path = vasp_sub_path
@@ -20,6 +32,7 @@ class VASPManager:
         self.files_to_copy = glob(os.path.join(self.structre_db_path, f"*{prefix}*"))
 
     def copy_and_enter_folder(self, prefix):
+        """ Copy files from the structure database to the current working directory and enter the folder """
         files_to_copy = glob(os.path.join(self.structre_db_path, f"*{prefix}*"))
         if not files_to_copy:
             print(f"No files found with prefix '{prefix}' in '{self.structre_db_path}'.")
@@ -33,6 +46,16 @@ class VASPManager:
         return True
 
 class VASPJobManager:
+    """
+    Class to manage VASP jobs
+
+    Args:
+    manager: VASPManager object
+    atomic_structure_manager: AtomicStructureManager object
+    opt_incar_path: path to the OPT INCAR file
+    md_incar_path: path to the MD INCAR file
+    user_name: user name for the VASP submission script 
+    """
     def __init__(self, manager, atomic_structure_manager, opt_incar_path, md_incar_path, user_name):
         self.manager = manager
         self.atomic_structure_manager = atomic_structure_manager
@@ -41,6 +64,7 @@ class VASPJobManager:
         self.user_name = user_name
 
     def change_magmom_incar(self, directory):
+        """ Change the MAGMOM and other magnetic parameters in the INCAR file """
         poscar_path = os.path.join(directory, "POSCAR")
         incar_path = os.path.join(directory, "INCAR")
         
@@ -78,6 +102,7 @@ class VASPJobManager:
             print(f"MAGMOM and other magnetic parameters updated in {incar_path}")
     
     def opt_operation(self):
+        """ Perform OPT operation """
         current_directory = os.getcwd()
         record_file_path = os.path.join(self.manager.work_path, "record.txt")  # Path for the log file
     
@@ -104,6 +129,7 @@ class VASPJobManager:
                 self.prepare_opt_folder(new_folder, opt_folder, poscar_path)
     
     def prepare_opt_folder(self, new_folder, opt_folder, poscar_path):
+        """ Prepare the OPT folder """
         shutil.move(poscar_path, os.path.join(new_folder, "POSCAR"))
         shutil.copy(os.path.join(new_folder, "POSCAR"), opt_folder)
         time.sleep(0.2)
@@ -116,6 +142,7 @@ class VASPJobManager:
         self.wait_and_submit(opt_folder, os.path.join(self.manager.work_path, "record.txt"))
     
     def md_operation(self):
+        """ Perform MD operation """
         current_directory = os.getcwd()
         record_file_path = os.path.join(self.manager.work_path, "record.txt")  # For logging
         folders = [os.path.join(current_directory, f) for f in os.listdir(current_directory) if os.path.isdir(f) and f.endswith('d')]
@@ -154,6 +181,7 @@ class VASPJobManager:
                         print(f"OPT results in {opt_folder} are not converged.")
                     
     def is_md_complete(self, md_folder_path):
+        """ Check if MD calculations are complete"""
         outcar_path = os.path.join(md_folder_path, 'OUTCAR')
         try:
             with open(outcar_path, 'r') as file:
@@ -165,6 +193,7 @@ class VASPJobManager:
         return False
 
     def prepare_and_submit_md_job(self, opt_folder, md_folder_path, record_file_path):
+        """ Prepare and submit MD job """
         os.makedirs(md_folder_path, exist_ok=True)
         shutil.copy(os.path.join(opt_folder, 'CONTCAR'), os.path.join(md_folder_path, 'POSCAR'))
         self.atomic_structure_manager.remove_zero_velocities(os.path.join(md_folder_path, 'POSCAR'))
@@ -225,10 +254,14 @@ class VASPJobManager:
 
 
 class AtomicStructureManager:
+    """
+    Class for managing atomic structures.
+    """
     def __init__(self):
         pass
 
     def check_atom_number(self, filepath):
+        """ Check if the number of atoms in the POSCAR file is greater than 300. """
         try:
             with open(filepath, 'r') as f:
                 lines = f.readlines()
@@ -260,7 +293,9 @@ class AtomicStructureManager:
             print(f"Error processing {poscar_path}: {e}")
 
 class ConvergenceChecker:
+    """ Class for checking convergence of calculations."""
     def check_convergence(self, directory):
+        """ Check if the calculation has converged. """
         outcar_path = os.path.join(directory, 'OUTCAR')
         try:
             with open(outcar_path, 'r') as file:
@@ -271,6 +306,7 @@ class ConvergenceChecker:
         return False
 
     def check_scf_convergence(self, directory):
+        """check if scf converged """
         outcar_path = os.path.join(directory, 'OUTCAR')
         oszicar_path = os.path.join(directory, 'OSZICAR')
         scf_converged = False
@@ -305,6 +341,7 @@ class ConvergenceChecker:
         return scf_converged
 
     def md_check(self, md_folder):
+        """ Check if the MD calculations have completed. """
         outcar_path = os.path.join(md_folder, 'OUTCAR')
         record_file_path = os.path.join(md_folder, "record.txt")  # Assuming each folder may have its own record file
         try:
@@ -326,33 +363,40 @@ class ConvergenceChecker:
                 record_file.write(f"{outcar_path} file not found in {md_folder}\n")
 
 class DPDataProcessor:
+    """
+    Class for processing DP data.
+
+    Attributes:
+        step (int): Step size for data processing.
+        test_size (float): Percentage of data to be used for testing.
+        prefix (str): Prefix for data files.
+        fmt (str): Format of data files.
+    """
     def __init__(self,step=1,test_size=0.1,prefix='./data',fmt='npy_single') -> None:
         self.step=step
         self.test_size=test_size
         self.prefix=prefix
         self.fmt=fmt
     def process(self):
+        """ Process DP data."""
         def find_folders_with_outcar(search_path='./POSCAR',label='OUTCAR'):
+            """ Find folders containing 'OUTCAR' files. """
             seen_folders = []  
-            # 遍历指定搜索路径下的所有OUTCAR文件  
+           # Traverse all 'OUTCAR' files under the specified search path.
             for outcar_path in glob(os.path.join(search_path, '**/MD/', label)):  
-            # 获取OUTCAR文件所在的文件夹路径  
+             # Get the folder path where the OUTCAR file resides.
                 folder_path = os.path.dirname(outcar_path)  
-            # 如果文件夹路径不是我们已经打印过的（避免重复打印）  
+            # If the folder path is not one that we have already processed (to avoid duplicate prints). 
                 if folder_path not in seen_folders:  
-                # 将文件夹路径添加到已打印的文件夹集合中  
+                  # Add the folder path to the collection of folders that have been processed.
                     seen_folders.append(folder_path)
             return seen_folders
         
         import re
         def extract_nonzero_elements(compound_string):
-            # 正则表达式匹配元素和数量
+            """ Extract non-zero elements from a compound string."""
             elements = re.findall(r'([A-Z][a-z]*)(\d+)', compound_string)
-            
-            # 过滤数量为0的元素
             nonzero_elements = [(element, int(quantity)) for element, quantity in elements if int(quantity) != 0]
-            
-            # 重组化学式
             formula = ''.join(f"{element}{quantity}" if quantity != 1 else element for element, quantity in nonzero_elements)         
             return formula
         
@@ -362,10 +406,10 @@ class DPDataProcessor:
             try:
                 data=dp.LabeledSystem(f'{i}/OUTCAR',fmt='vasp/outcar',step=int(self.step))
             except Exception as e:
-                print(i)
+                print(f"system {i} failed to load,please check the OUTCAR file")
                 continue
             ms.append(data)
-        # print(ms)
+        # split data into train and validation
         dataset=ms.train_test_split(test_size=float(self.test_size))
         if not os.path.exists(self.prefix):
             os.makedirs(self.prefix)
@@ -384,18 +428,20 @@ class DPDataProcessor:
             for i in dataset[1]:
                 name=extract_nonzero_elements(i.formula)
                 i.to_deepmd_npy(self.prefix + "/val"+f"/{name}")
+
         if self.fmt=='npy_mix':
             dataset[0].to_deepmd_npy_mixed(self.prefix + "/train")
             dataset[1].to_deepmd_npy_mixed(self.prefix + "/val")
+
         elif self.fmt=='lmdb':
-            # train=dataset[0].to_ase_structure(self.prefix + "/train")
-            # val=dataset[1].to_ase_structure(self.prefix + "/val")
+
             from fairchem.core.preprocessing import AtomsToGraphs
             import lmdb
             import pickle
             import torch
             import numpy as np
             def tolmdb(file,data):
+                """ Convert data to lmdb format. note: this format is justed for s2ef task with fairchem package."""
                 a2g = AtomsToGraphs(
                 max_neigh=50,
                 radius=6,
@@ -410,12 +456,15 @@ class DPDataProcessor:
                     name=extract_nonzero_elements(i.formula)
                     ase_data=i.to_ase_structure()
 
+                # simple devide atom according to z coordinate, 1 for subsurface, 2 for surface
                     b=ase_data[0].positions[:,2]
                     # b=a[1]['pos'][:,2]
                     c1= b<np.max(b)-2 
                     c2= np.min(b)+2< b
                     c=c1 & c2
                     tag=np.where(c,1,2)
+
+
                     # ase_data[0].set_tags(tag)
                     data_objects = a2g.convert_all(ase_data, disable_tqdm=True)
                     # tags = ase_data[0].get_tags()
@@ -447,6 +496,7 @@ class DPDataProcessor:
 
 
 def parse_arguments():
+    """ Parse command-line arguments. """
     parser = argparse.ArgumentParser(description="Manage VASP calculations and processing with various commands.")
     parser.add_argument("prefix", help="Prefix for files to process.")
     parser.add_argument("operation", choices=["opt", "md", "optcheck", "mdcheck", "dpdata",'plot'], help="Specify the operation to perform.")
@@ -454,6 +504,7 @@ def parse_arguments():
     return parser.parse_args()
 
 def read_parameters_from_file(file_path="input"):
+    """ Read parameters from a file. """
     parameters = {}
     with open(file_path, 'r') as file:
         for line in file:
@@ -478,6 +529,7 @@ def read_parameters_from_file(file_path="input"):
     )
 
 def main():
+    """ Main function. the entry for the program."""
     args = parse_arguments()
     work_path, opt_incar_path, md_incar_path, vasp_sub_path, structre_db_path, max_jobs, sleep_time, user_name, step,test_size, dataset_prefix, dataset_fmt, plot_name = read_parameters_from_file()
     manager = VASPManager(work_path, vasp_sub_path, structre_db_path, int(max_jobs), int(sleep_time), user_name, args.prefix)
