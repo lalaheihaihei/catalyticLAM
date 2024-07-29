2d01f33102e61c0890a78673579fdf592d33f544
h757062840 <houpengfei@master.gengzi>
2024-07-29 16:56:12 +0800

    add RNET scripts

---
+++ postworkflow/RNET/utilis/material_db.py
@@ -0,0 +1,18 @@
+type1=[ 
+        {'name': 'Li', 'mp_id': 'mp-135', 'ads': mol.db},
+        {'name': 'Na', 'mp_id': 'mp-127', 'ads': mol.db},
+        {'name': 'Mg', 'mp_id': 'mp-153', 'ads': mol.db},
+        {'name': 'Fe', 'mp_id': 'mp-13', 'ads': mol.db},
+        {'name': 'Co', 'mp_id': 'mp-54', 'ads': mol.db},
+        {'name': 'Ni', 'mp_id': 'mp-23', 'ads': mol.db},
+        {'name': 'Cu', 'mp_id': 'mp-30', 'ads': mol.db},
+        {'name': 'Zn', 'mp_id': 'mp-79', 'ads': mol.db},
+        {'name': 'Ru', 'mp_id': 'mp-33', 'ads':mol.db},
+        {'name': 'Rh', 'mp_id': 'mp-74', 'ads':mol.db},
+        {'name': 'Pd', 'mp_id': 'mp-2', 'ads': mol.db},
+        {'name': 'Ag', 'mp_id': 'mp-124', 'ads':mol.db},
+        {'name': 'Ir', 'mp_id': 'mp-101', 'ads':mol.db},
+        {'name': 'Pt', 'mp_id': 'mp-126', 'ads':mol.db},
+        {'name': 'Au', 'mp_id': 'mp-81', 'ads': mol.db},
+        {'name': 'Al', 'mp_id': 'mp-134', 'ads':mol.db}
+        ]
